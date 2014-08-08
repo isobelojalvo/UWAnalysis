f4498d067f233c433ada7ec180089458eadbb418
isobelojalvo <ojalvo@wisc.edu>
2014-08-08 17:23:34 -0500

    remove some verbose output

--- RecoTools/plugins/ESTauProducer.cc
+++ RecoTools/plugins/ESTauProducer.cc
@@ -30,26 +30,12 @@ ESTauProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
     using namespace reco;
 
     std::auto_ptr<std::vector<pat::Tau> > out(new std::vector<pat::Tau> );
-    //edm::refhelper::FindUsingAdvance<std::vector<pat::Tau> > srcH;
-
-    //edm::Handle<edm::View<pat::Tau> > srcH;
-    //edm::Handle<edm::Ptr<edm::Ptr<reco::PFCandidate>>> srcH;
-
-    //edm::Handle<std::vector<pat::Tau> > srcH;
     edm::Handle<pat::TauCollection > srcH;
-    //edm::Handle<edm::Ptr<reco::PFCandidateRef>>srcH;
 
     edm::Handle<reco::GenParticleCollection> genParticleCollection;
-    // Handle<reco::GenParticleCollection> 
-
-
-   std::cout<<"1 ESTauProducer"<<std::endl;
 
     bool tauPresent = false;    
     bool genParticlesPresent = false;     
-      //if(evt.getByLabel(srcGenParticles_,genParticleCollection))
-	//
-
       
     if(iEvent.getByLabel(genParticles_,genParticleCollection)){
       ////first check if there is a tau somewhere in the collection
@@ -61,60 +47,58 @@ ESTauProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
 	}
       }
     }
-    std::cout<<"2 ESTauProducer"<<std::endl;
+
     if(iEvent.getByLabel(src_,srcH)) 
-      std::cout<<"3 ESTauProducer"<<std::endl;
     unsigned int i = 0 ;
-    //while(srcH->isNonnull()){
     for(unsigned int i=0;i<srcH->size();++i) {
-	 std::cout<<"HERE3"<<std::endl;
-	 pat::Tau object = srcH->at(i);
-	//std::std::cout << " original object(" << i << "): Pt = " << object.pt(); 
-	//	    << " eta = " << object.eta() << ", phi = " << object.phi() << std::endl;
+      
+      pat::Tau object = srcH->at(i);
+      //std::std::cout << " original object(" << i << "): Pt = " << object.pt(); 
+      //	    << " eta = " << object.eta() << ", phi = " << object.phi() << std::endl;
 	
-	smearingModule->smear(object);
-	float initialpt = object.pt();
-	float initialpx = object.px();
-	float initialpy = object.py();
-	float initialpz = object.pz();
-	float initialeta = object.eta();
-	float initialphi = object.phi();
-	float initialMass = object.mass();
-	object.addUserFloat("preESMass",initialMass);
-	object.addUserFloat("preESpt",initialpt);
-	object.addUserFloat("preESpx",initialpx);
-	object.addUserFloat("preESpy",initialpy);
-	object.addUserFloat("preESpz",initialpz);
-	object.addUserFloat("preESeta",initialeta);
-	object.addUserFloat("preESphi",initialphi);
-
+      smearingModule->smear(object);
+      float initialpt = object.pt();
+      float initialpx = object.px();
+      float initialpy = object.py();
+      float initialpz = object.pz();
+      float initialeta = object.eta();
+      float initialphi = object.phi();
+      float initialMass = object.mass();
+      object.addUserFloat("preESMass",initialMass);
+      object.addUserFloat("preESpt",initialpt);
+      object.addUserFloat("preESpx",initialpx);
+      object.addUserFloat("preESpy",initialpy);
+      object.addUserFloat("preESpz",initialpz);
+      object.addUserFloat("preESeta",initialeta);
+      object.addUserFloat("preESphi",initialphi);
+      
+      
+      math::XYZTLorentzVector finalP4 = object.p4();
+      if(genParticlesPresent) {
 	
-	math::XYZTLorentzVector finalP4 = object.p4();
-	if(genParticlesPresent) {
-
-	  if(object.decayMode()==1||object.decayMode()==2||object.decayMode()==10){
-	    if(object.decayMode()==1||object.decayMode()==2){
-	      finalP4 = (object.p4())* (1.012);
-	    }
-	    else if(object.decayMode()==10){
-	      finalP4 = (object.p4())* (1.012);
-	      
-	    }
+	if(object.decayMode()==1||object.decayMode()==2||object.decayMode()==10){
+	  if(object.decayMode()==1||object.decayMode()==2){
+	    finalP4 = (object.p4())* (1.012);
+	  }
+	  else if(object.decayMode()==10){
+	    finalP4 = (object.p4())* (1.012);
 	    
-	    object.addUserFloat("ESmass",finalP4.mass());
 	  }
-
-	  if(smearConstituents_&&tauPresent) {
-	    object.setP4(finalP4);
+	  
+	  object.addUserFloat("ESmass",finalP4.mass());
 	  }
+	
+	if(smearConstituents_&&tauPresent) {
+	  object.setP4(finalP4);
 	}
+      }
+      
+      //std::cout << "smeared object(" << i << ") : decay mode = " << object.decayMode() <<" : Pt = " << object.pt() << std::endl ;  
+      //<< " eta = " << object.eta() << ", phi = " << object.phi() << std::endl;
 	
-	//std::cout << "smeared object(" << i << ") : decay mode = " << object.decayMode() <<" : Pt = " << object.pt() << std::endl ;  
-	//<< " eta = " << object.eta() << ", phi = " << object.phi() << std::endl;
-	
-	
-	out->push_back(object);
-	i++;
+      
+      out->push_back(object);
+      i++;
     }
     iEvent.put(out);
 }
