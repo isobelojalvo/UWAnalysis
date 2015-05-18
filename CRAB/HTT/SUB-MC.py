fd540bbdadb2ae254cff76e472d8ac308d925f78
Laura Dodd <ldodd@cern.ch>
2015-05-18 08:46:04 -0500

    Adding HTT submission directory

---
+++ CRAB/HTT/SUB-MC.py
@@ -0,0 +1,117 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("ANALYSIS")
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+
+process.GlobalTag.globaltag = 'PHYS14_25_V1'
+
+#added in etau and mutau triggers
+from UWAnalysis.Configuration.tools.analysisToolsMiniAod import *
+defaultReconstructionMC(process,'HLT',
+                      [
+						'HLT_Ele22_eta2p1_WP85_Gsf_LooseIsoPFTau20_v1', #etau
+                                                'HLT_Ele27_eta2p1_WP85_Gsf_v1', #etau
+						'HLT_IsoMu17_eta2p1_LooseIsoPFTau20_v1', #mutau
+                                                'HLT_IsoMu24_eta2p1_IterTrk02_v1' #mutau
+                      ])
+
+                      
+
+#EventSelection
+process.load("UWAnalysis.Configuration.MiniAodAnalysis_cff")
+
+process.metCalibration.applyCalibration = cms.bool(False)
+
+process.eventSelectionMT = cms.Path(process.selectionSequenceMT)
+process.eventSelectionET = cms.Path(process.selectionSequenceET)
+#Systematic Shifts 1sigma
+process.eventSelectionMTTauUp    = createSystematics(process,process.selectionSequenceMT,'TauUp',1.0,1.0,1.03,0,1.0)
+process.eventSelectionMTTauDown  = createSystematics(process,process.selectionSequenceMT,'TauDown',1.0,1.0,0.97,0,1.0)
+process.eventSelectionMTJetUp    = createSystematics(process,process.selectionSequenceMT,'JetUp',1.0,1.0,1.0,1,1.0)
+process.eventSelectionMTJetDown  = createSystematics(process,process.selectionSequenceMT,'JetDown',1.0,1.0,1.0,-1,1.0)
+
+process.eventSelectionETTauUp    = createSystematics(process,process.selectionSequenceET,'TauUp',1.00,1.0,1.03,0,1.0)
+process.eventSelectionETTauDown  = createSystematics(process,process.selectionSequenceET,'TauDown',1.0,1.0,0.97,0,1.0)
+process.eventSelectionMTJetUp    = createSystematics(process,process.selectionSequenceMT,'JetUp',1.0,1.0,1.0,1,1.0)
+process.eventSelectionMTJetDown  = createSystematics(process,process.selectionSequenceMT,'JetDown',1.0,1.0,1.0,-1,1.0)
+
+
+
+createGeneratedParticles(process,
+                         'genDaughters',
+                          [
+                           "keep++ pdgId = {Z0}",
+                           "keep pdgId = {tau+}",
+                           "keep pdgId = {tau-}",
+                           "keep pdgId = {mu+}",
+                           "keep pdgId = {mu-}",
+                           "keep pdgId = 6",
+                           "keep pdgId = -6",
+                           "keep pdgId = 11",
+                           "keep pdgId = -11",
+                          ]
+)
+
+
+createGeneratedParticles(process,
+                         'genTauCands',
+                          [
+                           "keep pdgId = {tau+} & mother.pdgId()= {Z0}",
+                           "keep pdgId = {tau-} & mother.pdgId() = {Z0}"
+                          ]
+)
+
+
+from UWAnalysis.Configuration.tools.ntupleToolsMiniAod import addMuTauEventTree
+addMuTauEventTree(process,'muTauEventTree')
+addMuTauEventTree(process,'muTauEventTreeFinal','diTausOS','diMuonsSorted')
+
+
+from UWAnalysis.Configuration.tools.ntupleToolsMiniAod import addEleTauEventTree
+addEleTauEventTree(process,'eleTauEventTree')
+addEleTauEventTree(process,'eleTauEventTreeFinal','eleTausOS','osDiElectrons')
+
+addEventSummary(process,True,'MT','eventSelectionMT')
+addEventSummary(process,True,'ET','eventSelectionET')
+
+
+#Final trees afor shapes after shifts
+addMuTauEventTree(process,'muTauEventTreeTauUp','diTausSortedTauUp','diMuonsSortedTauUp')
+addMuTauEventTree(process,'muTauEventTreeTauDown','diTausSortedTauDown','diMuonsSortedTauDown')
+addMuTauEventTree(process,'muTauEventTreeFinalTauUp','diTausOSTauUp','diMuonsSortedTauUp')
+addMuTauEventTree(process,'muTauEventTreeFinalTauDown','diTausOSTauDown','diMuonsSortedTauDown')
+addMuTauEventTree(process,'muTauEventTreeJetUp','diTausSortedJetUp','diMuonsSortedJetUp')
+addMuTauEventTree(process,'muTauEventTreeJetDown','diTausSortedJetDown','diMuonsSortedJetDown')
+addMuTauEventTree(process,'muTauEventTreeFinalJetUp','diTausOSJetUp','diMuonsSortedJetUp')
+addMuTauEventTree(process,'muTauEventTreeFinalJetDown','diTausOSJetDown','diMuonsSortedJetDown')
+
+addEleTauEventTree(process,'eleTauEventTreeTauUp','eleTausSortedTauUp','osDiElectronsTauUp')
+addEleTauEventTree(process,'eleTauEventTreeTauDown','eleTausSortedTauDown','osDiElectronsTauDown')
+addEleTauEventTree(process,'eleTauEventTreeFinalTauUp','eleTausOSTauUp','osDiElectronsTauUp')
+addEleTauEventTree(process,'eleTauEventTreeFinalTauDown','eleTausOSTauDown','osDiElectronsTauDown')
+addEleTauEventTree(process,'eleTauEventTreeJetUp','eleTausSortedJetUp','osDiElectronsJetUp')
+addEleTauEventTree(process,'eleTauEventTreeJetDown','eleTausSortedJetDown','osDiElectronsJetDown')
+addEleTauEventTree(process,'eleTauEventTreeFinalJetUp','eleTausOSJetUp','osDiElectronsJetUp')
+addEleTauEventTree(process,'eleTauEventTreeFinalJetDown','eleTausOSJetDown','osDiElectronsJetDown')
+
+
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+       $inputFileNames
+		),
+		inputCommands=cms.untracked.vstring(
+						'keep *',
+		)
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+#process.TFileService.fileName=cms.string("$outputFileName")
+process.TFileService = cms.Service(
+    "TFileService",
+    fileName = cms.string("$outputFileName")
+)
---
+++ CRAB/HTT/merge.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+mkdir /nfs_scratch/$USER/ztt_shifts
+cd /nfs_scratch/$USER/ztt_shifts
+
+
+#only run merge or weight. Do not set both to 1.
+merge=0;
+weight=0;
+
+
+if [ $merge -eq 1 ]
+    then
+    /cms/sw/farmout/mergeFiles --reuse-cache-files ggH.root /hdfs/store/user/$USER/ggHtautau-SUB-MCH &
+    /cms/sw/farmout/mergeFiles --reuse-cache-files vbfH.root /hdfs/store/user/$USER/vbfHtautau-SUB-MCH &
+    /cms/sw/farmout/mergeFiles --reuse-cache-files QCD.root /hdfs/store/user/$USER/QCD_15to3000Flat-SUB-MC &
+    /cms/sw/farmout/mergeFiles --reuse-cache-files ZTT.root /hdfs/store/user/$USER/dyJetsLL-SUB-MC &
+    /cms/sw/farmout/mergeFiles --reuse-cache-files WW.root /hdfs/store/user/$USER/WW-SUB-MC 
+    nohup /cms/sw/farmout/mergeFiles --reuse-cache-files TTJets.root /hdfs/store/user/$USER/TT_MSCKM-SUB-MC &
+    nohup /cms/sw/farmout/mergeFiles --reuse-cache-files TT.root /hdfs/store/user/$USER/TT_Tune4C-SUB-MC &
+    nohup /cms/sw/farmout/mergeFiles --reuse-cache-files WJets.root /hdfs/store/user/$USER/WJets-SUB-MC &
+    nohup /cms/sw/farmout/mergeFiles --reuse-cache-files WZ.root /hdfs/store/user/$USER/WZJets-SUB-MC &
+    nohup /cms/sw/farmout/mergeFiles --reuse-cache-files t.root /hdfs/store/user/$USER/t_tW-SUB-MC &
+    nohup /cms/sw/farmout/mergeFiles --reuse-cache-files tBar.root /hdfs/store/user/$USER/tBar_tW-SUB-MC &
+fi
+
+if [ $weight -eq 1 ]
+    then
+    EventWeightsIterative outputFile='ggH.root'     weight=43.9    histoName='MT/results'
+    EventWeightsIterative outputFile='vbfH.root'    weight=3.7     histoName='MT/results'
+    EventWeightsIterative outputFile='QCD.root'     weight=125000000000   histoName='MT/results'
+    EventWeightsIterative outputFile='ZTT.root'     weight=6025    histoName='MT/results'
+    EventWeightsIterative outputFile='TTJets.root'  weight=832     histoName='MT/results'
+    EventWeightsIterative outputFile='TT.root'      weight=832     histoName='MT/results'
+    EventWeightsIterative outputFile='WJets.root'   weight=20509   histoName='MT/results'
+    EventWeightsIterative outputFile='WZ.root'      weight=1.634   histoName='MT/results'
+    EventWeightsIterative outputFile='t.root'       weight=35.6    histoName='MT/results'
+    EventWeightsIterative outputFile='tBar.root'    weight=35.6    histoName='MT/results'
+    EventWeightsIterative outputFile='WW.root'     weight=110.8    histoName='MT/results'
+    hadd VV.root WZ.root t.root tBar.root
+fi
+
+
