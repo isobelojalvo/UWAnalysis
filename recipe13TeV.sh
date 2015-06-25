9941ca437397be396573f1c5aaf90421dde917a4
Laura Dodd <ldodd@cern.ch>
2015-06-25 08:33:57 -0500

    updated recipe

--- recipe13TeV.sh
+++ recipe13TeV.sh
@@ -2,20 +2,20 @@
 
 
 #source environment.sh
-set -o errexit
-set -o nounset
+#set -o errexit
+#set -o nounset
 
 
 #cd ../
 #git clone https://github.com/cms-analysis/TauAnalysis-CandidateTools.git TauAnalysis/CandidateTools
 #pushd $CMSSW_BASE/src/TauAnalysis/CandidateTools
 #git checkout TauAnalysis-CandidateTools-V00-01-04s
-#pushd $CMSSW_BASE/src
-
-git clone git@github.com:veelken/SVFit_standalone.git TauAnalysis/SVfitStandalone
-cd TauAnalysis/SVfitStandalone
-git checkout svFit_2015Apr03
 pushd $CMSSW_BASE/src
+
+#git clone git@github.com:veelken/SVFit_standalone.git TauAnalysis/SVfitStandalone
+#cd TauAnalysis/SVfitStandalone
+#git checkout svFit_2015Apr03
+#pushd $CMSSW_BASE/src
 
 #cp -r /cms/laura/UWAnalysis13/CMSSW_7_3_3/src/AnalysisDataFormats/ .
 cp /cms/ojalvo/mssm8TeV/CMSSW_5_3_7/src/UWAnalysis/Configuration/data/factoryJetRegNewGenJets* UWAnalysis/Configuration/data/.
@@ -28,8 +28,11 @@ cp /cms/ojalvo/mssm8TeV/CMSSW_5_3_7/src/UWAnalysis/Configuration/data/factoryJet
 
 
 #cutbased ID
-git cms-merge-topic ikrav:egm_id_74X_v0
-
+#git cms-merge-topic 9003 #this is the version that is in CMSSW_7_4_X
+#rm -rf RecoEgamma/ElectronIdentification/data
+#git clone https://github.com/cms-data/RecoEgamma-ElectronIdentification.git RecoEgamma/ElectronIdentification/data
+#rm -rf RecoEgamma/PhotonIdentification/data
+#git clone https://github.com/cms-data/RecoEgamma-PhotonIdentification.git RecoEgamma/PhotonIdentification/data
 
 #MVA MET
 git cms-addpkg RecoMET/METPUSubtraction
