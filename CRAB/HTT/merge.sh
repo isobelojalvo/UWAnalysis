26038f6a3bae3fe6f7b46e2bcbfd12914932c909
Laura Dodd <ldodd@cern.ch>
2015-06-30 07:29:19 -0500

    Updating merge again

--- CRAB/HTT/merge.sh
+++ CRAB/HTT/merge.sh
@@ -26,7 +26,7 @@ if [ $merge -eq 1 ]
     /cms/sw/farmout/mergeFiles --reuse-cache-files WW.root /hdfs/store/user/$USER/WW-SUB-MC 
     /cms/sw/farmout/mergeFiles --reuse-cache-files ZZ.root /hdfs/store/user/$USER/ZZ-SUB-MC 
     nohup /cms/sw/farmout/mergeFiles --reuse-cache-files TTJets.root /hdfs/store/user/$USER/TTJets-SUB-MC &
-    nohup /cms/sw/farmout/mergeFiles --reuse-cache-files TT.root /hdfs/store/user/$USER/TT_Tune4C-SUB-MC &
+    #nohup /cms/sw/farmout/mergeFiles --reuse-cache-files TT.root /hdfs/store/user/$USER/TT_Tune4C-SUB-MC &
     nohup /cms/sw/farmout/mergeFiles --reuse-cache-files WJets.root /hdfs/store/user/$USER/WJets-SUB-MC &
     nohup /cms/sw/farmout/mergeFiles --reuse-cache-files WZ.root /hdfs/store/user/$USER/WZJets-SUB-MC &
     nohup /cms/sw/farmout/mergeFiles --reuse-cache-files t.root /hdfs/store/user/$USER/t_tW-SUB-MC &
@@ -48,14 +48,14 @@ if [ $weight -eq 1 ]
     EventWeightsIterative outputFile='em300infQCD.root'     weight=9000   histoName='MT/results'
     EventWeightsIterative outputFile='ZJets.root'     weight=6025    histoName='MT/results'
     EventWeightsIterative outputFile='TTJets.root'  weight=832     histoName='MT/results'
-    EventWeightsIterative outputFile='TT.root'      weight=832     histoName='MT/results'
+    #EventWeightsIterative outputFile='TT.root'      weight=832     histoName='MT/results'
     EventWeightsIterative outputFile='WJets.root'   weight=20509   histoName='MT/results'
     EventWeightsIterative outputFile='WZ.root'      weight=1.634   histoName='MT/results'
     EventWeightsIterative outputFile='ZZ.root'      weight=15.4   histoName='MT/results'
     EventWeightsIterative outputFile='t.root'       weight=35.6    histoName='MT/results'
     EventWeightsIterative outputFile='tBar.root'    weight=35.6    histoName='MT/results'
     EventWeightsIterative outputFile='WW.root'     weight=110.8    histoName='MT/results'
-    hadd VV.root WZ.root t.root tBar.root
+    hadd VV.root WZ.root WW.root ZZ.root t.root tBar.root
 fi
 
 
