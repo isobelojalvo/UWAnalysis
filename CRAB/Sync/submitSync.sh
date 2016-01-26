a79f4c9ae311c25319f4280a6746fa219571e985
Laura Dodd <ldodd@cern.ch>
2016-01-26 17:12:59 -0600

    rmoving old files

--- CRAB/Sync/submitSync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-voms-proxy-init --voms cms --valid 100:00
-
-
-######HIGGS SAMPLES #############
-farmoutAnalysisJobs  $1 --input-dir=root://cmsxrootd.fnal.gov/ --assume-input-files-exist --input-file-list=SUSY.txt  syncNtupleEle35 $CMSSW_BASE $CMSSW_BASE/src/UWAnalysis/CRAB/Sync/SUB-ELE.py
-farmoutAnalysisJobs  $1 --input-dir=root://cmsxrootd.fnal.gov/ --assume-input-files-exist --input-file-list=SUSY.txt syncNtupleMu35 $CMSSW_BASE $CMSSW_BASE/src/UWAnalysis/CRAB/Sync/SUB-MU.py
