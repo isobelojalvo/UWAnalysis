68bf7d0d0b364bd80debe36d38f9ab8da55cb3ea
Laura Dodd <ldodd@cern.ch>
2016-01-29 08:41:29 -0600

    removing outdated root files

--- weightMSSM.sh
+++ weightMSSM.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #mkdir /nfs_scratch/$USER/htt_weighted5
 #cp /nfs_scratch/$USER/htt_unweighted5/* /nfs_scratch/$USER/htt_weighted5/.
-cd /nfs_scratch/$USER/htt_weighted5_susy/
+cd /nfs_scratch/$USER/htt_weighted5/
 
 
-weightGGA=0;
+weightGGA=1;
 weightBBA=1;
 
 
@@ -16,12 +16,14 @@ if [ $weightGGA -eq 1 ]
     EventWeightsIterativeGen outputFile='susyggH_100.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
     EventWeightsIterativeGen outputFile='susyggH_110.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
     EventWeightsIterativeGen outputFile='susyggH_120.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
+    EventWeightsIterativeGen outputFile='susyggH_130.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
     EventWeightsIterativeGen outputFile='susyggH_140.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
     EventWeightsIterativeGen outputFile='susyggH_160.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
     EventWeightsIterativeGen outputFile='susyggH_180.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
     EventWeightsIterativeGen outputFile='susyggH_200.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
     EventWeightsIterativeGen outputFile='susyggH_250.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
     EventWeightsIterativeGen outputFile='susyggH_400.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
+    EventWeightsIterativeGen outputFile='susyggH_450.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
     EventWeightsIterativeGen outputFile='susyggH_500.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
     EventWeightsIterativeGen outputFile='susyggH_600.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
     EventWeightsIterativeGen outputFile='susyggH_700.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
@@ -49,6 +51,7 @@ if [ $weightBBA -eq 1 ]
     EventWeightsIterativeGen outputFile='susybbH_100.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
     EventWeightsIterativeGen outputFile='susybbH_110.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
     EventWeightsIterativeGen outputFile='susybbH_120.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
+    EventWeightsIterativeGen outputFile='susybbH_130.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
     EventWeightsIterativeGen outputFile='susybbH_140.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
     EventWeightsIterativeGen outputFile='susybbH_160.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
     EventWeightsIterativeGen outputFile='susybbH_180.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
@@ -57,6 +60,7 @@ if [ $weightBBA -eq 1 ]
     EventWeightsIterativeGen outputFile='susybbH_300.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
     EventWeightsIterativeGen outputFile='susybbH_350.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
     EventWeightsIterativeGen outputFile='susybbH_400.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
+    EventWeightsIterativeGen outputFile='susybbH_450.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
     EventWeightsIterativeGen outputFile='susybbH_500.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
     EventWeightsIterativeGen outputFile='susybbH_600.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
     EventWeightsIterativeGen outputFile='susybbH_700.root'     weight=1    histoName='MT/results' sumHistoName='sumweights/genWeights'
