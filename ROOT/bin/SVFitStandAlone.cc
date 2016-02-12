e84c992d7f3f5b60027bdf2d1f614c4b3000e9e0
Tyler Ruggles <truggles@wisc.edu>
2016-02-12 11:53:20 +0100

    Working in interactive with new FSA ntuples as of today

--- ROOT/bin/SVFitStandAlone.cc
+++ ROOT/bin/SVFitStandAlone.cc
@@ -63,7 +63,6 @@ int main (int argc, char* argv[])
 
 void readdir(TDirectory *dir, optutl::CommandLineParser parser, char TreeToUse[]) 
 {
-  int j = 0;
   TDirectory *dirsav = gDirectory;
   TIter next(dir->GetListOfKeys());
   TKey *key;
@@ -82,7 +81,17 @@ void readdir(TDirectory *dir, optutl::CommandLineParser parser, char TreeToUse[]
     }
     else if(obj->IsA()->InheritsFrom(TTree::Class())) {
       TTree *t = (TTree*)obj;
-      float svFitMass = -1;
+      float svFitMass = -10;
+      float svFitPt = -10;
+      float svFitEta = -10;
+      float svFitPhi = -10;
+      float svFitMET = -10;
+      //TBranch *newBranch = t->Branch(parser.stringValue("branch").c_str(),&svFitMass,(parser.stringValue("branch")+"/F").c_str());
+      TBranch *newBranch1 = t->Branch("m_sv", &svFitMass, "m_sv/F");
+      TBranch *newBranch2 = t->Branch("pt_sv", &svFitPt, "pt_sv/F");
+      TBranch *newBranch3 = t->Branch("eta_sv", &svFitEta, "eta_sv/F");
+      TBranch *newBranch4 = t->Branch("phi_sv", &svFitPhi, "phi_sv/F");
+      TBranch *newBranch5 = t->Branch("met_sv", &svFitMET, "met_sv/F");
       float pt1;
       float eta1;
       float phi1;
@@ -104,49 +113,104 @@ void readdir(TDirectory *dir, optutl::CommandLineParser parser, char TreeToUse[]
       // define MET covariance
       TMatrixD covMET(2, 2);
       //ele/mu variables
-      //svFitStandalone::kDecayType decayType1 = svFitStandalone::kUndefinedDecayType; //svFitStandalone::kTauToElecDecay
-      svFitStandalone::kDecayType decayType1 = svFitStandalone::kTauToHadDecay; //svFitStandalone::kTauToElecDecay
-      svFitStandalone::kDecayType decayType2 = svFitStandalone::kTauToHadDecay; //svFitStandalone::kTauToElecDecay
-      float mass1 = 0.139;
-      //Should mass2 depend on hadronic tau decay mode?
-      float mass2 = 0.139;
-	  mass1 = 0.51100e-3;
-	  mass2 = 105.658e-3;
-	  decayType2 = svFitStandalone::kTauToMuDecay;
-	  decayType1 = svFitStandalone::kTauToElecDecay;
-      //determine if electron or muon
-    ////  if(std::string(TreeToUse).find("muTauEvent")!= std::string::npos){
-	////decayType1 = svFitStandalone::kTauToMuDecay;
-	////mass1 = 105.658e-3;
-    ////  }
-    ////  else if(std::string(TreeToUse).find("eleTauEvent")!= std::string::npos){
-	////decayType1 = svFitStandalone::kTauToElecDecay;
-	////mass1 = 0.51100e-3;
-    ////  }
-    ////  else{
-	////std::cout<<"TreeToUse "<< std::string(TreeToUse)<<" does not match muTauEvent or eleTauEvent... Skipping!!"<<std::endl;
-	////continue;
-    ////  }
+      svFitStandalone::kDecayType decayType1 = svFitStandalone::kUndefinedDecayType; //svFitStandalone::kTauToElecDecay
+      svFitStandalone::kDecayType decayType2 = svFitStandalone::kUndefinedDecayType; //svFitStandalone::kTauToElecDecay
+      // Both masses should depend on decay mode and particle?
+      float mass1;
+      float mass2;
+      std::string channel = "x";
+      if(std::string(TreeToUse).find("muTauEvent")!= std::string::npos){
+	decayType1 = svFitStandalone::kTauToMuDecay;
+    decayType2 = svFitStandalone::kTauToHadDecay; 
+	mass1 = 105.658e-3;
+	mass2 = 0.13957;
+    channel = "mt";
+      }
+      else if(std::string(TreeToUse).find("eleTauEvent")!= std::string::npos){
+	decayType1 = svFitStandalone::kTauToElecDecay;
+    decayType2 = svFitStandalone::kTauToHadDecay;
+	mass1 = 0.51100e-3;
+	mass2 = 0.13957;
+    channel = "et";
+      }
+      //else if(parser.stringValue("outputFile").find("_em.root") != std::string::npos){
+      else if(std::string(TreeToUse).find("em")!= std::string::npos){
+        //std::cout<< parser.stringValue("outputFile").c_str() << std::endl;
+        std::cout<< "EMu sample" <<std::endl;
+	decayType1 = svFitStandalone::kTauToElecDecay;
+	decayType2 = svFitStandalone::kTauToMuDecay;
+	mass1 = 0.51100e-3;
+	mass2 = 105.658e-3;
+    channel = "em";
+      }
+      //else if(parser.stringValue("outputFile").find("_tt.root") != std::string::npos){
+      else if(std::string(TreeToUse).find("tt")!= std::string::npos){
+        //std::cout<< parser.stringValue("outputFile").c_str() << std::endl;
+        std::cout<< "Double Hadronic sample" <<std::endl;
+    decayType1 = svFitStandalone::kTauToHadDecay;
+    decayType2 = svFitStandalone::kTauToHadDecay;
+	mass1 = 0.13957;
+	mass2 = 0.13957;
+    channel = "tt";
+      }
+      else{
+	std::cout<<"TreeToUse "<< std::string(TreeToUse)<<" does not match muTauEvent or eleTauEvent or 'em' or 'tt'... Skipping!!"<<std::endl;
+	continue;
+      }
       
       //MET, MET Covariance, lepton objects,
       TBranch *pt1branch;
-      TBranch *newBranch = t->Branch(parser.stringValue("branch").c_str(),&svFitMass,(parser.stringValue("branch")+"/F").c_str());
-      newBranch->SetFile("svfitout.root:/input/muTauEventTree");
+      //TBranch *newBranch = t->Branch(parser.stringValue("branch").c_str(),&svFitMass,(parser.stringValue("branch")+"/F").c_str());
+      //newBranch->SetFile("svfitout.root:/input/muTauEventTree");
 
-      t->SetBranchAddress("pt_1",&pt1,&pt1branch);
-      t->SetBranchAddress("eta_1",&eta1);
-      t->SetBranchAddress("phi_1",&phi1);
-      t->SetBranchAddress("pt_2",&pt2);
-      t->SetBranchAddress("eta_2",&eta2);
-      t->SetBranchAddress("phi_2",&phi2);
-      t->SetBranchAddress("t1DecayMode",&decayMode);
-      t->SetBranchAddress("t2DecayMode",&decayMode2);
-      t->SetBranchAddress("covMatrix00",&covMatrix00);
-      t->SetBranchAddress("covMatrix01",&covMatrix01);
-      t->SetBranchAddress("covMatrix10",&covMatrix10);
-      t->SetBranchAddress("covMatrix11",&covMatrix11);
-      t->SetBranchAddress("mvamet",&met);
-      t->SetBranchAddress("mvametphi",&metphi);
+      if(channel=="tt") {
+        t->SetBranchAddress("t1Pt",&pt1,&pt1branch);
+        t->SetBranchAddress("t1Eta",&eta1);
+        t->SetBranchAddress("t1Phi",&phi1);
+        t->SetBranchAddress("t2Pt",&pt2);
+        t->SetBranchAddress("t2Eta",&eta2);
+        t->SetBranchAddress("t2Phi",&phi2);
+        t->SetBranchAddress("t1DecayMode",&decayMode);
+        t->SetBranchAddress("t2DecayMode",&decayMode2);
+        t->SetBranchAddress("t1_t2_TTpairMvaMetCovMatrix00",&covMatrix00);
+        t->SetBranchAddress("t1_t2_TTpairMvaMetCovMatrix01",&covMatrix01);
+        t->SetBranchAddress("t1_t2_TTpairMvaMetCovMatrix10",&covMatrix10);
+        t->SetBranchAddress("t1_t2_TTpairMvaMetCovMatrix11",&covMatrix11);
+        t->SetBranchAddress("t1_t2_TTpairMvaMet",&met);
+        t->SetBranchAddress("t1_t2_TTpairMvaMetPhi",&metphi);
+      }
+
+      else if(channel=="em") {
+        t->SetBranchAddress("ePt",&pt1,&pt1branch);
+        t->SetBranchAddress("eEta",&eta1);
+        t->SetBranchAddress("ePhi",&phi1);
+        t->SetBranchAddress("mPt",&pt2);
+        t->SetBranchAddress("mEta",&eta2);
+        t->SetBranchAddress("mPhi",&phi2);
+        t->SetBranchAddress("e_m_EMpairMvaMetCovMatrix00",&covMatrix00);
+        t->SetBranchAddress("e_m_EMpairMvaMetCovMatrix01",&covMatrix01);
+        t->SetBranchAddress("e_m_EMpairMvaMetCovMatrix10",&covMatrix10);
+        t->SetBranchAddress("e_m_EMpairMvaMetCovMatrix11",&covMatrix11);
+        t->SetBranchAddress("e_m_EMpairMvaMet",&met);
+        t->SetBranchAddress("e_m_EMpairMvaMetPhi",&metphi);
+      }
+
+      else {
+        t->SetBranchAddress("pt_1",&pt1,&pt1branch);
+        t->SetBranchAddress("eta_1",&eta1);
+        t->SetBranchAddress("phi_1",&phi1);
+        t->SetBranchAddress("pt_2",&pt2);
+        t->SetBranchAddress("eta_2",&eta2);
+        t->SetBranchAddress("phi_2",&phi2);
+        t->SetBranchAddress("t1DecayMode",&decayMode);
+        t->SetBranchAddress("t2DecayMode",&decayMode2);
+        t->SetBranchAddress("covMatrix00",&covMatrix00);
+        t->SetBranchAddress("covMatrix01",&covMatrix01);
+        t->SetBranchAddress("covMatrix10",&covMatrix10);
+        t->SetBranchAddress("covMatrix11",&covMatrix11);
+        t->SetBranchAddress("mvamet",&met);
+        t->SetBranchAddress("mvametphi",&metphi);
+      }
       
       printf("Found tree -> weighting\n");
       for(Int_t i=0;i<t->GetEntries();++i)
@@ -172,44 +236,151 @@ void readdir(TDirectory *dir, optutl::CommandLineParser parser, char TreeToUse[]
 	  ////  mass2 = 0.13957*2;
 	  ////else if(decayMode==3)
 	  ////  mass2 = 0.13957*3;
+      if(channel=="et" || channel=="mt"){
+	    if(decayMode==0)
+	      mass2 = 0.13957;
+	    else if(decayMode==1)
+	      mass2 = 0.13957*2;
+	    else if(decayMode==3)
+	      mass2 = 0.13957*3;}
+      if(channel=="tt"){
+	    if(decayMode==0)
+	      mass1 = 0.13957;
+	    else if(decayMode==1)
+	      mass1 = 0.13957*2;
+	    else if(decayMode==3)
+	      mass1 = 0.13957*3;
+	    if(decayMode2==0)
+	      mass2 = 0.13957;
+	    else if(decayMode2==1)
+	      mass2 = 0.13957*2;
+	    else if(decayMode2==3)
+	      mass2 = 0.13957*3;}
 
 	  covMET[0][0] =  covMatrix00;
 	  covMET[1][0] =  covMatrix10;
 	  covMET[0][1] =  covMatrix01;
 	  covMET[1][1] =  covMatrix11;
-	  ///comment me out 
-	  ////covMET[0][0] =  787.352;
-	  ////covMET[1][0] = -178.63;
-	  ////covMET[0][1] = -178.63;
-	  ////covMET[1][1] =  179.545;
-	  /// to here once we have real covMET... 0's cause SVFit to SegFault.
-	  //std::cout<<"Dir: "<<(dir->GetPath())<<std::endl;
-	  //std::cout<<"DirSav: "<<(dirsav->GetPath())<<std::endl;
-	  //std::cout<<"gDirectory: "<<(gDirectory->GetPath())<<std::endl;
-	  if((decayMode==0||decayMode==1||decayMode==10)&&(decayMode2==0||decayMode2==1||decayMode2==10)){
+
+
+	  if((decayMode==0||decayMode==1||decayMode==10)&&(channel=="et"||channel=="mt")){
 	    // define lepton four vectors
 	    std::vector<svFitStandalone::MeasuredTauLepton> measuredTauLeptons;
 	    // check if electron or muon
 	    measuredTauLeptons.push_back(
-		 //svFitStandalone::MeasuredTauLepton(decayType1, pt1, eta1,  phi1, mass1,decayMode)
-		 svFitStandalone::MeasuredTauLepton(decayType1, pt1, eta1,  phi1, mass1)
-					 ); // tau -> electron decay (Pt, eta, phi, mass)
+		  svFitStandalone::MeasuredTauLepton(decayType1, pt1, eta1,  phi1, mass1)
+		  			 ); // tau -> electron decay (Pt, eta, phi, mass)
 	    
 	    measuredTauLeptons.push_back(
-		 //svFitStandalone::MeasuredTauLepton(decayType2,  pt2, eta2, phi2,  mass2, decayMode2)
-		 svFitStandalone::MeasuredTauLepton(decayType2,  pt2, eta2, phi2,  mass2)
+	  	  svFitStandalone::MeasuredTauLepton(decayType2,  pt2, eta2, phi2,  mass2, decayMode)
+	  				 ); // tau -> 1prong0pi0 hadronic decay (Pt, eta, phi, mass, pat::Tau.decayMode())
+        
+	    //std::cout<<"Here 0"<<std::endl;
+	    SVfitStandaloneAlgorithm algo(measuredTauLeptons, measuredMETx, measuredMETy, covMET, 1);
+	    //std::cout<<"Here 1"<<std::endl;
+	    algo.addLogM(false);  
+	    //std::cout<<"Here 2"<<std::endl;
+	    //algo.shiftVisPt(true, inputFile_visPtResolution);
+	    //std::cout<<"Here 3"<<std::endl;
+	    algo.integrateMarkovChain();
+	    //std::cout<<"Here 4"<<std::endl;
+	    svFitMass = algo.getMass(); // return value is in units of GeV
+	    svFitPt = algo.pt();
+	    svFitEta = algo.eta();
+	    svFitPhi = algo.phi();
+	    svFitMET = algo.fittedMET().Rho();
+	    //std::cout<<"Here 5"<<std::endl;
+	    if ( algo.isValidSolution() ) {
+	      std::cout << "found mass = " << svFitMass << std::endl;
+        }
+	    else {
+	      std::cout << "sorry -- status of NLL is not valid [" << algo.isValidSolution() << "]" << std::endl;
+	    }
+      } // eTau / muTau
+
+	  else if(channel=="em"){
+	    // define lepton four vectors
+	    std::vector<svFitStandalone::MeasuredTauLepton> measuredTauLeptons;
+	    // check if electron or muon
+	      measuredTauLeptons.push_back(
+		   svFitStandalone::MeasuredTauLepton(decayType1, pt1, eta1,  phi1, mass1)
+		  			 ); // tau -> electron decay (Pt, eta, phi, mass)
+	    
+	      measuredTauLeptons.push_back(
+		   svFitStandalone::MeasuredTauLepton(decayType2,  pt2, eta2, phi2, mass2)
 					 ); // tau -> 1prong0pi0 hadronic decay (Pt, eta, phi, mass, pat::Tau.decayMode())
 
+	    SVfitStandaloneAlgorithm algo(measuredTauLeptons, measuredMETx, measuredMETy, covMET, 1);
+	    algo.addLogM(false);  
+	    //algo.shiftVisPt(true, inputFile_visPtResolution);
+	    algo.integrateMarkovChain();
+	    svFitMass = algo.getMass(); // return value is in units of GeV
+	    svFitPt = algo.pt();
+	    svFitEta = algo.eta();
+	    svFitPhi = algo.phi();
+	    svFitMET = algo.fittedMET().Rho();
+	    if ( algo.isValidSolution() ) {
+	      std::cout << "found mass = " << svFitMass << std::endl;
+        }
+	    else {
+	      std::cout << "sorry -- status of NLL is not valid [" << algo.isValidSolution() << "]" << std::endl;
+	    }
+      } // eMu
+
+
+	  //else if((decayMode==0||decayMode==1||decayMode==10)&&(decayMode2==0||decayMode2==1||decayMode2==10)&&channel=="tt"){
+	  else if(channel=="tt"){
+	    // define lepton four vectors
+	    std::vector<svFitStandalone::MeasuredTauLepton> measuredTauLeptons;
+	    // check if electron or muon
+	      measuredTauLeptons.push_back(
+		   svFitStandalone::MeasuredTauLepton(decayType1, pt1, eta1,  phi1, mass1, decayMode)
+		  			 ); // tau -> electron decay (Pt, eta, phi, mass)
 	    
-	    svFitMass = runSVFit(measuredTauLeptons, measuredMETx, measuredMETy, covMET, 0);
-	    dir->cd();
-	  }
-	  else
-	    svFitMass = 0;
+	      measuredTauLeptons.push_back(
+	  	   svFitStandalone::MeasuredTauLepton(decayType2,  pt2, eta2, phi2,  mass2, decayMode2)
+	  				 ); // tau -> 1prong0pi0 hadronic decay (Pt, eta, phi, mass, pat::Tau.decayMode())
+
+	    SVfitStandaloneAlgorithm algo(measuredTauLeptons, measuredMETx, measuredMETy, covMET, 1);
+	    algo.addLogM(false);  
+	    //algo.shiftVisPt(true, inputFile_visPtResolution);
+	    algo.integrateMarkovChain();
+	    svFitMass = algo.getMass(); // return value is in units of GeV
+	    svFitPt = algo.pt();
+	    svFitEta = algo.eta();
+	    svFitPhi = algo.phi();
+	    svFitMET = algo.fittedMET().Rho();
+	    if ( algo.isValidSolution() ) {
+	      std::cout << "found mass = " << svFitMass << std::endl;
+        }
+	    else {
+	      std::cout << "sorry -- status of NLL is not valid [" << algo.isValidSolution() << "]" << std::endl;
+	    }
+      } // Double Hadronic (TT)
+
+	  else {
+	    svFitMass = -100;
+        svFitPt = -100;
+        svFitEta = -100;
+        svFitPhi = -100;
+        svFitMET = -100;
+      }
 	  
-	  newBranch->Fill();
+      std::cout << "Mass: " << svFitMass << " Pt: " << svFitPt << " Eta: " << svFitEta;
+      std::cout << " Phi: " << svFitPhi << " MET: " << svFitMET << std::endl;
+	  newBranch1->Fill();
+	  newBranch2->Fill();
+	  newBranch3->Fill();
+	  newBranch4->Fill();
+	  newBranch5->Fill();
+      svFitMass = -100;
+      svFitPt = -100;
+      svFitEta = -100;
+      svFitPhi = -100;
+      svFitMET = -100;
 	}
 
+	  
       t->Write("",TObject::kOverwrite);
       strcpy(TreeToUse,stringA) ;
 
